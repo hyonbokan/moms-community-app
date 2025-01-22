a70be070954a6316f8c46624f843b91b6366214a
hyonbokan <mkkanhb@gmail.com>
2025-01-22 12:07:15 +0900

    init commit

---
+++ mca/src/test/java/com/momscommunity/mca/McaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momscommunity.mca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
